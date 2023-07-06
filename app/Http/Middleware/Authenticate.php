5cf4ca78b0a4afa0159625343ef1439f7dc1b873
YuyaYamazi <kaimomo12345@gmail.com>
2023-07-07 03:22:03 +0900

    guard設定

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,14 +4,28 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson()) {
+            if(Route::is('owner.*')) {
+                return route($this->owner_route);
+            }
+            elseif(Route::is('admin.*')) {
+                return route($this->admin_route);
+            }
+            else {
+                return route($this->user_route);
+            }
+         }
     }
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,6 +10,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
+
+    private const GUARD_USER = 'users';
+    private const GUARD_OWNER = 'owners';
+    private const GUARD_ADMIN = 'admin';
     /**
      * Handle an incoming request.
      *
@@ -17,12 +21,25 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
-        $guards = empty($guards) ? [null] : $guards;
+        // $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        // foreach ($guards as $guard) {
+        //     if (Auth::guard($guard)->check()) {
+        //         return redirect(RouteServiceProvider::HOME);
+        //     }
+        // }
+        
+        // User
+        if(Auth::guard(self::GUARD_USER)->check() && $request->routeIs('user.*')) {
+            return redirect(RouteServiceProvider::HOME);
+        }
+        // Owner
+        if(Auth::guard(self::GUARD_OWNER)->check() && $request->routeIs('owner.*')) {
+            return redirect(RouteServiceProvider::OWNER_HOME);
+        }
+         // Admin
+         if(Auth::guard(self::GUARD_ADMIN)->check() && $request->routeIs('admin.*')) {
+            return redirect(RouteServiceProvider::ADMIN_HOME);
         }
 
         return $next($request);
