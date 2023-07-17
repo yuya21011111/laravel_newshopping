d93ad5b704f058074b72de8c154a9dd795d0f35b
YuyaYamazi <kaimomo12345@gmail.com>
2023-07-18 04:43:13 +0900

    4041編集

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -10,8 +10,27 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                    @foreach($shops as $shop)
-                     {{ $shop->id }}<br>
-                     {{ $shop->name }}
+                   <div class="w-1/4 p -4">
+                    <a href="{{ route('owner.shops.edit',['shop' => $shop->id]) }}">
+                    <div class="border rounded-md p-4">
+                        <div class="mb-4">
+                            @if($shop->is_selling)
+                            <span class="border p-2 rounded-md bg-blue-400 text-white">販売中
+                            @else
+                            <span class="border p-2 rounded-md bg-red-400 text-white">販売中
+                            @endif
+                        </div>
+                     <div class="text-xl">{{ $shop->name }}</div>
+                     <div>
+                        @if(empty($shop->filename))
+                          <img src="{{ asset('images/no_images.png') }}">
+                        @else
+                          <img src="{{ asset('storage/shops' . $shop->filename) }}">
+                        @endif
+                     </div>
+                    </div>
+                    </a>
+                   </div>
                    @endforeach
                 </div>
             </div>
