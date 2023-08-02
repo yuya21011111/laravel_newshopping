adaf81c18aee374e148cfcf604908f4b98a7a062
YuyaYamazi <kaimomo12345@gmail.com>
2023-08-03 03:28:05 +0900

    Cart購入動作テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,10 +4,26 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function add(Request $request) {
-        dd($request);
+        $itemInCart = Cart::where('user_id',Auth::id())
+        ->where('product_id',$request->product_id)->first();
+
+        if($itemInCart){
+            $itemInCart->quantity += $request->quantity;
+            $itemInCart->save();
+        } else {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity
+            ]);
+        }
+        dd('カートテスト');
+        return redirect()->route('user.cart.index');
     }
 }
