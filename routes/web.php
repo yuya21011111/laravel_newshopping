7cd3d5ab90847aed84e4b9afa5c426ca486d5ff0
YuyaYamazi <kaimomo12345@gmail.com>
2023-08-03 15:05:21 +0900

    cart.index作成

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth:users')
 });
 
 Route::prefix('cart')->middleware('auth:users')->group(function(){
+    Route::get('/',[CartController::class,'index'])->name('cart.index');
     Route::post('/add',[CartController::class,'add'])->name('cart.add');
 });
 
